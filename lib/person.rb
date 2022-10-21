8696828cc4e728d1ff1f9aa2626bf9076363513d
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-10-21 19:38:02 +0300

    complete

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,25 @@
 class Person
+     # setter method
+    def name=(name)
+        @name = name
+    end
+
+    def job=(job)
+        @job = job
+    end
+
+    # getter method
+
+    def name
+        @name
+    end
+
+    def job
+        @job
+    end
 
 end
+
+beyonce = Person.new
+beyonce.name=("Beyonce")
+beyonce.job=("Singer")
